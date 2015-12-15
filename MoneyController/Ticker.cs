074d93fc7fad79c5ee32f5476b55ede1e48a61ca
missytoy <missytoy@gmail.com>
2015-12-15 12:18:15 +0200

    fixed dateTime in AddExpensePage.xaml and AddIncomePage.xaml

---
+++ MoneyController/Ticker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+
+namespace MoneyController
+{
+    public class DateTimeTaker 
+    {
+        public string Now
+        {
+            get { return DateTime.Now.ToString("dd-MMM-yyyy hh:mm tt"); }
+        }
+    }
+}
