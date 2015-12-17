411d482195616e09f8bf2f2586927c260ca868b8
missytoy <missytoy@gmail.com>
2015-12-17 20:20:46 +0200

    moved all pages to folder Pages

---
+++ MoneyController/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MoneyController.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyController.ViewModels
+{
+   public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<ExpenseItem> expenses;
+
+        public IEnumerable<ExpenseItem> Expences
+        {
+            get
+            {
+                if (expenses == null)
+                {
+                    expenses = new ObservableCollection<ExpenseItem>();
+                }
+
+                return expenses;
+            }
+            set
+            {
+                if (expenses == null)
+                {
+                    expenses = new ObservableCollection<ExpenseItem>();
+                }
+
+                expenses.Clear();
+                foreach (var item in value)
+                {
+                    expenses.Add(item);
+                }
+            }
+        }
+    }
+}
