36c9af6dac156bf893cf207e03a0d33908dfd0cc
Aleksander Angelov <Obelixx@users.noreply.github.com>
2015-12-20 19:45:53 +0200

    swipe added to main Pages/MainPage.xaml

--- MoneyController/Pages/MainPage.xaml.cs
+++ MoneyController/Pages/MainPage.xaml.cs
@@ -20,74 +20,128 @@ namespace MoneyController
 {
     public sealed partial class MainPage : Page
     {
-        public object MainPagePage;
-        public AddExpensePage AddExpensePagePage;
-        public AddIncomePage AddIncomePagePage;
-        public AnalyticsPage AnalyticsPagePage;
-        //public Page OptionsPagePage;
-        
+        private object MainPagePage;
+        private AddExpensePage AddExpensePagePage;
+        private AddIncomePage AddIncomePagePage;
+        private AnalyticsPage AnalyticsPagePage;
+        private Point initialpoint;
+        string currentPage;
 
         public MainPage()
         {
             this.InitializeComponent();
             MainPagePage = MainPageFrame.Content;
+            this.currentPage = "MainPage";
             this.AddExpensePagePage = new AddExpensePage(this);
             this.AddIncomePagePage = new AddIncomePage(this);
             this.AnalyticsPagePage = new AnalyticsPage(this);
             //this.OptionsPagePage = new OptionsPage(this);
+
+        }
+
+        private void MainPage_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            this.ManipulationStarted -= MainPage_ManipulationStarted;
+            this.ManipulationDelta -= MainPage_ManipulationDelta;
+            this.ManipulationCompleted -= MainPage_ManipulationCompleted;
+        }
+
+        void MainPage_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            initialpoint = e.Position;
+        }
+
+        void MainPage_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (e.IsInertial)
+            {
+                Point currentpoint = e.Position;
+
+                if (currentpoint.X - initialpoint.X >= 300)
+                {
+                    System.Diagnostics.Debug.WriteLine("Swipe Right");
+
+                    if (currentPage == "MainPage")
+                    {
+                        OnAddIncomeButtonClick(null, null);
+                    }
+                    else if (currentPage == "AddExpensePage")
+                    {
+                        NavigateToMainPage();
+                    }
+                }
+                if (currentpoint.X - initialpoint.X <= -300)
+                {
+                    System.Diagnostics.Debug.WriteLine("Swipe Left");
+
+                    if (currentPage == "MainPage")
+                    {
+                        OnAddExpenseButtonClick(null, null);
+                    }
+                    else if (currentPage == "AddIncomePage")
+                    {
+                        NavigateToMainPage();
+                    }
+                }
+                e.Complete();
+                System.Diagnostics.Debug.WriteLine(currentPage);
+            }
+
         }
 
         public void NavigateToMainPage()
         {
+            currentPage = "MainPage";
             MainPageFrame.Content = MainPagePage;
         }
 
         private void OnAddIncomeButtonClick(object sender, RoutedEventArgs e)
         {
+            this.currentPage = "AddIncomePage";
             MainPageFrame.Content = this.AddIncomePagePage;
         }
 
         private void OnAddExpenseButtonClick(object sender, RoutedEventArgs e)
         {
+            this.currentPage = "AddExpensePage";
             MainPageFrame.Content = this.AddExpensePagePage;
         }
 
         private void OnAnalyticsButtonClick(object sender, RoutedEventArgs e)
         {
+            this.currentPage = "AnalyticsPage";
             MainPageFrame.Content = this.AnalyticsPagePage; //TODO:  //charts for day,for month, for year...
         }
 
         private void OnOptionsButtonClick(object sender, RoutedEventArgs e)
         {
-            //TODO: MainPageFrame.Content = this.OptionsPage; //stop notifications
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateToMainPage();
+            this.currentPage = "OptionsPage";
+            //TODO: MainPageFrame.Content = this.OptionsPagePage; //stop notifications
+            this.TopAppBar.IsOpen = !this.TopAppBar.IsOpen;
         }
 
         private void Home_Click(object sender, RoutedEventArgs e)
         {
-
-            this.Frame.Navigate(typeof(MainPage));
+            NavigateToMainPage();
+            this.TopAppBar.IsOpen = !this.TopAppBar.IsOpen;
         }
 
         private void AddExpense_Click(object sender, RoutedEventArgs e)
         {
-            MainPageFrame.Content = this.AddExpensePagePage;
+            OnAddExpenseButtonClick(null, null);
+            this.TopAppBar.IsOpen = !this.TopAppBar.IsOpen;
         }
 
         private void AddIncome_Click(object sender, RoutedEventArgs e)
         {
-            MainPageFrame.Content = this.AddIncomePagePage;
+            OnAddIncomeButtonClick(null, null);
+            this.TopAppBar.IsOpen = !this.TopAppBar.IsOpen;
         }
 
         private void Analytics_Click(object sender, RoutedEventArgs e)
         {
-            MainPageFrame.Content = this.AnalyticsPagePage;
+            OnAnalyticsButtonClick(null, null);
+            this.TopAppBar.IsOpen = !this.TopAppBar.IsOpen;
         }
-
-
     }
 }
